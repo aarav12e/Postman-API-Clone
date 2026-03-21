4cc027d5266965b8382f426a6f187e057f3fccdb
Aarav Kumar <aarav12f@gmail.com>
2026-03-21 18:00:26 +0530

    test: Add basic test for the App component.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Postman app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Postman/i);
   expect(linkElement).toBeInTheDocument();
 });
